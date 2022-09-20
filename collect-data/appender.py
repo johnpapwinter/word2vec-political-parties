bb458f4c9ad492980eb26603114d089eb81a2962
John Papwinter <johnpapahimonas@yahoo.com>
2022-09-20 19:21:32 +0300

    minor fixes

--- collect-data/appender.py
+++ collect-data/appender.py
@@ -25,7 +25,8 @@ def Lemmas(text):
     return text2
 '''
 
-def Pitchless(text):
+
+def pitchless(text):
     clean = re.sub('[άὰᾶἀἄἂἆἁἅἃἇᾳᾴᾲᾷᾀᾄᾂᾆᾁᾅᾃᾇ]', 'α', text)
     clean = re.sub('[έὲἐἔἒἑἕἓ]', 'ε', clean)
     clean = re.sub('[ϊΐίὶῖἰἴἶἱἵἳἷ]', 'ι', clean)
@@ -36,12 +37,14 @@ def Pitchless(text):
     clean = re.sub('[ῤῥ]', 'ρ', clean)
     return clean
 
-def No_punct(text):
-    clean = re.sub('['+string.punctuation+']', ' ', text)
+
+def no_punctuation(text):
+    clean = re.sub('[' + string.punctuation + ']', ' ', text)
     clean = re.sub('[«»“”’…–]', ' ', clean)
     return clean
 
-def Party_Catcher(text):
+
+def party_catcher(text):
     clean = text
     clean = re.sub(r'\bσυ ρι ζα\b', 'συριζα', clean)
     clean = re.sub(r'\bχ α\b', 'χα', clean)
@@ -54,10 +57,10 @@ def Party_Catcher(text):
     clean = re.sub(r'\bη π α\b', 'ηπα', clean)
     return clean
 
-		
-def Normalize_Names(text):
+
+def normalize_names(text):
     normal = text
-    #NORMALIZE SYRIZA
+    # NORMALIZE SYRIZA
     normal = re.sub(r'\bτσιπρα\b', 'τσιπρας', normal)
     normal = re.sub(r'\bαλεξη\b', 'αλεξης', normal)
     normal = re.sub(r'\bπρωθυπουργο\b|\bπρωθυπουργου\b', 'πρωθυπουργος', normal)
@@ -66,7 +69,7 @@ def Normalize_Names(text):
     normal = re.sub(r'\bτσακαλωτο\b', 'τσακαλωτος', normal)
     normal = re.sub(r'\bευκλειδη\b', 'ευκλειδης', normal)
     normal = re.sub(r'\bπαπαδημουλη\b', 'παπαδημουλης', normal)
-    normal = re.sub(r'\bεφης\b', 'εφη', normal) #Αχτσιογλου άκλιτο
+    normal = re.sub(r'\bεφης\b', 'εφη', normal)  # Αχτσιογλου άκλιτο
     normal = re.sub(r'\bσταθακη\b', 'σταθακης', normal)
     normal = re.sub(r'\bγιωργου\b', 'γιωργος', normal)
     normal = re.sub(r'\bκουρουμπλη\b', 'κουρουμπλης', normal)
@@ -77,50 +80,50 @@ def Normalize_Names(text):
     normal = re.sub(r'\bτζανακοπουλο\b', 'τζανακοπουλος', normal)
     normal = re.sub(r'\bδημητρη\b', 'δημητρης', normal)
     normal = re.sub(r'\bβουτση\b', 'βουτσης', normal)
-    normal = re.sub(r'\bρανιας\b', 'ρανια', normal) #Σβιγκου άκλιτο
+    normal = re.sub(r'\bρανιας\b', 'ρανια', normal)  # Σβιγκου άκλιτο
     normal = re.sub(r'\bπολακη\b', 'πολακης', normal)
     normal = re.sub(r'\bπαυλου\b|\bπαυλο\b', 'παυλος', normal)
     normal = re.sub(r'\bφωτη\b', 'φωτης', normal)
     normal = re.sub(r'\bκουβελη\b', 'κουβελης', normal)
-    #NORMALIZE ANEL
+    # NORMALIZE ANEL
     normal = re.sub(r'\bανεξαρτητων ελληνων\b|\bανεξαρτητοι ελληνες\b|\bανεξαρτητους ελληνες\b', 'ανελ', normal)
     normal = re.sub(r'\bκαμμενου\b|\bκαμμενο\b', 'καμμενος', normal)
     normal = re.sub(r'\bπανου\b|\bπανο\b', 'πανος', normal)
-    normal = re.sub(r'\bμανταλενας\b', 'μανταλενα', normal) #Μυτιληναιου άκλιτο
+    normal = re.sub(r'\bμανταλενας\b', 'μανταλενα', normal)  # Μυτιληναιου άκλιτο
     normal = re.sub(r'\bκουντουρας\b', 'κουντουρα', normal)
     normal = re.sub(r'\bελενας\b', 'ελενα', normal)
     normal = re.sub(r'\bζουραρη\b', 'ζουραρης', normal)
-    #NORMALIZE ND
+    # NORMALIZE ND
     normal = re.sub(r'\bνεας δημοκρατιας\b|\bνεα δημοκρατια\b', 'νδ', normal)
     normal = re.sub(r'\bμητσοτακη\b', 'μητσοτακης', normal)
     normal = re.sub(r'\bκυριακο\b|\bκυριακου\b', 'κυριακος', normal)
     normal = re.sub(r'\bπροεδρο\b|\bπροεδρου\b', 'προεδρος', normal)
     normal = re.sub(r'\bγεωργιαδη\b', 'γεωργιαδης', normal)
     normal = re.sub(r'\bαδωνι\b|\bαδωνη\b|\bαδωνις\b', 'αδωνης', normal)
-    normal = re.sub(r'\bντορας\b', 'ντορα', normal) #Μπακογιάννη άκλιτο
+    normal = re.sub(r'\bντορας\b', 'ντορα', normal)  # Μπακογιάννη άκλιτο
     normal = re.sub(r'\bσαμαρα\b', 'σαμαρας', normal)
     normal = re.sub(r'\bαντωνη\b', 'αντωνης', normal)
     normal = re.sub(r'\bβοριδη\b', 'βοριδης', normal)
     normal = re.sub(r'\bμακη\b', 'μακης', normal)
-    normal = re.sub(r'\bμαριας\b', 'μαρια', normal) #Σπυράκη άκλιτο
+    normal = re.sub(r'\bμαριας\b', 'μαρια', normal)  # Σπυράκη άκλιτο
     normal = re.sub(r'\bκαραμανλη\b', 'καραμανλης', normal)
     normal = re.sub(r'\bκωστα\b', 'κωστας', normal)
     normal = re.sub(r'\bκικιλια\b', 'κικιλιας', normal)
     normal = re.sub(r'\bβασιλη\b', 'βασιλης', normal)
     normal = re.sub(r'\bβαγγελη\b', 'βαγγελης', normal)
     normal = re.sub(r'\bμειμαρακη\b', 'μειμαρακης', normal)
-    #NORMALIZE PASOK
-    normal = re.sub(r'\bφωφης\b', 'φωφη', normal) #Γεννηματά άκλιτο
+    # NORMALIZE PASOK
+    normal = re.sub(r'\bφωφης\b', 'φωφη', normal)  # Γεννηματά άκλιτο
     normal = re.sub(r'\bευαγγελο\b|\bευαγγελου\b', 'ευαγγελος', normal)
     normal = re.sub(r'\bβενιζελου\b|\bβενιζελο\b', 'βενιζελος', normal)
     normal = re.sub(r'\bλοβερδο\b', 'λοβερδος', normal)
     normal = re.sub(r'\bανδρεα\b', 'ανδρεας', normal)
-    #NORMALIZE KKE
+    # NORMALIZE KKE
     normal = re.sub(r'\bκουτσουμπα\b', 'κουτσουμπας', normal)
     normal = re.sub(r'\bδημητρη\b', 'δημητρης', normal)
     normal = re.sub(r'\bκανελλης\b', 'δκανελλη', normal)
     normal = re.sub(r'\bλιανας\b', 'λιανα', normal)
-    #NORMALIZE XA
+    # NORMALIZE XA
     normal = re.sub(r'\bχρυσης αυγης\b|\bχα\b', 'χρυση αυγη', normal)
     normal = re.sub(r'\bμιχαλολιακου\b', 'μιχαλολιακος', normal)
     normal = re.sub(r'\bνικολα\b|\bνικολαου\b', 'νικολας', normal)
@@ -132,11 +135,11 @@ def Normalize_Names(text):
     normal = re.sub(r'\bδιεθνες νομισματικο ταμειο\b|\bδιεθνους νομισματικου ταμειου\b', 'δντ', normal)
     normal = re.sub(r'\bτροικας\b', 'τροικα', normal)
     return normal
-    
-        
-def Hellenize_Names(text):
+
+
+def hellenize_names(text):
     clean = text
-    #POLITICS
+    # POLITICS
     clean = re.sub(r'\bseverna makedonija\b', 'σεβερνα μακεντονιγια', clean)
     clean = re.sub(r'\bgorna makedonija\b', 'γκορνα μακεντονιγια', clean)
     clean = re.sub(r'\bnato\b', 'νατο', clean)
@@ -144,7 +147,7 @@ def Hellenize_Names(text):
     clean = re.sub(r'\bimf\b', 'δντ', clean)
     clean = re.sub(r'\bcommission\b', 'κομισιον', clean)
     clean = re.sub(r'\beurogroup\b', 'γιουρογκρουπ', clean)
-    #COMPANIES
+    # COMPANIES
     clean = re.sub(r'\bnovartis\b', 'νοβαρτις', clean)
     clean = re.sub(r'\bexxonmobil\b|\bexonmobil\b', 'εξομομπιλ', clean)
     clean = re.sub(r'\beni\b', 'ενι', clean)
@@ -152,7 +155,7 @@ def Hellenize_Names(text):
     clean = re.sub(r'\brepsol\b', 'ρεπσολ', clean)
     clean = re.sub(r'\bcosco\b', 'κοσκο', clean)
     clean = re.sub(r'\beldorado\b', 'ελντοραντο', clean)
-    #MILITARY
+    # MILITARY
     clean = re.sub(r'\bbelharra\b|\bbelh arra\b', 'μπελχαρα', clean)
     clean = re.sub(r'\bfremm\b', 'φρεμμ', clean)
     clean = re.sub(r'\bmeko\b', 'μεκο', clean)
@@ -163,7 +166,7 @@ def Hellenize_Names(text):
     clean = re.sub(r'\bs 300\b', 'εστριακοσια', clean)
     clean = re.sub(r'\bmirage 2000\b', 'μιραζ', clean)
     return clean
-		
+
 
 stoplist = open("C:/Users/johnp/Desktop/Dissertation/STOP.txt", 'r', encoding='utf-8')
 stoplist = stoplist.readlines()
@@ -171,75 +174,79 @@ stop = []
 for i in stoplist:
     i = re.sub('\ufeff', '', i)
     i = i.rstrip()
-    i = Pitchless(i)
+    i = pitchless(i)
     stop.append(i)
 
-#print("Loaded")
+# print("Loaded")
 
 doc_complete = []
 for name in glob.glob("C:/Users/johnp/Desktop/Dissertation/ANEL Website/*"):
-#for name in glob.glob("C:/Users/johnp/Desktop/Dissertation/KKE Website/*"):
-#for name in glob.glob("C:/Users/johnp/Desktop/Dissertation/ND Website/*"):
-#for name in glob.glob("C:/Users/johnp/Desktop/Dissertation/PASOK Website/*"):
-#for name in glob.glob("C:/Users/johnp/Desktop/Dissertation/SYRIZA Website/*"):
-#for name in glob.glob("C:/Users/johnp/Desktop/Dissertation/XA Website/*"):
-    file = open (name, 'r', encoding = 'utf-8')
+    # for name in glob.glob("C:/Users/johnp/Desktop/Dissertation/KKE Website/*"):
+    # for name in glob.glob("C:/Users/johnp/Desktop/Dissertation/ND Website/*"):
+    # for name in glob.glob("C:/Users/johnp/Desktop/Dissertation/PASOK Website/*"):
+    # for name in glob.glob("C:/Users/johnp/Desktop/Dissertation/SYRIZA Website/*"):
+    # for name in glob.glob("C:/Users/johnp/Desktop/Dissertation/XA Website/*"):
+    file = open(name, 'r', encoding='utf-8')
     file = file.read()
     file = re.sub('\ufeff', ' ', file)
     file = file.rstrip()
-    file = re.sub(r'''(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))''', " ", file)
-    file = No_punct(file)
-    #file = Lemmas(file)
+    file = re.sub(
+        r'''(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))''',
+        " ", file)
+    file = no_punctuation(file)
+    # file = Lemmas(file)
     file = file.lower()
     file = re.sub(r'\bδιαβάστε περισσότερα\b', ' ', file)
     file = re.sub(r'\bδελτίο τύπου\b', ' ', file)
-    file = Party_Catcher(file)
-    file = Hellenize_Names(file)
-    file = Pitchless(file)
+    file = party_catcher(file)
+    file = hellenize_names(file)
+    file = pitchless(file)
     file = " ".join([i for i in file.split() if i not in stop])
     file = re.sub('[^α-ω]', ' ', file)
     file = " ".join(file.split())
-    #file = word_tokenize(file)
-    #doc_complete.append(file)
-    file = Normalize_Names(file)
-    #file = " ".join([i for i in file.split() if len(i)>1 and i not in exception])
+    # file = word_tokenize(file)
+    # doc_complete.append(file)
+    file = normalize_names(file)
+    # file = " ".join([i for i in file.split() if len(i)>1 and i not in exception])
     all_prop.write(file)
-    
-#print("Website data OK")
+
+# print("Website data OK")
 
 for name in glob.glob("C:/Users/johnp/Desktop/Dissertation/Twitter Data/ANEL/*"):
-#for name in glob.glob("C:/Users/johnp/Desktop/Dissertation/Twitter Data/KKE/*"):
-#for name in glob.glob("C:/Users/johnp/Desktop/Dissertation/Twitter Data/ND/*"):
-#for name in glob.glob("C:/Users/johnp/Desktop/Dissertation/Twitter Data/PASOK/*"):
-#for name in glob.glob("C:/Users/johnp/Desktop/Dissertation/Twitter Data/SYRIZA/*"):
-#for name in glob.glob("C:/Users/johnp/Desktop/Dissertation/Twitter Data/XA/*"):
-    file = open (name, 'r', encoding = 'utf-8')
+    # for name in glob.glob("C:/Users/johnp/Desktop/Dissertation/Twitter Data/KKE/*"):
+    # for name in glob.glob("C:/Users/johnp/Desktop/Dissertation/Twitter Data/ND/*"):
+    # for name in glob.glob("C:/Users/johnp/Desktop/Dissertation/Twitter Data/PASOK/*"):
+    # for name in glob.glob("C:/Users/johnp/Desktop/Dissertation/Twitter Data/SYRIZA/*"):
+    # for name in glob.glob("C:/Users/johnp/Desktop/Dissertation/Twitter Data/XA/*"):
+    file = open(name, 'r', encoding='utf-8')
     file = file.readlines()
     for tweet in file:
-        #tweet = re.sub(r'\[datetime\.datetime\([0-9]+,\s[0-9]+,\s[0-9]+,\s[0-9]+,\s[0-9]+,\s[0-9]+\),\s\'', '', tweet)
+        # tweet = re.sub(r'\[datetime\.datetime\([0-9]+,\s[0-9]+,\s[0-9]+,\s[0-9]+,\s[0-9]+,\s[0-9]+\),\s\'', '', tweet)
         tweet = re.sub(r'\bdatetime.datetime\b', "", tweet)
-        tweet = re.sub('[0-9]|['+string.punctuation+']',"", tweet)
+        tweet = re.sub('[0-9]|[' + string.punctuation + ']', "", tweet)
         tweet = re.sub('\ufeff', '', tweet)
         tweet = tweet.lstrip()
         if tweet.startswith('RT'):
             pass
         else:
-            tweet = re.sub(r'''(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))''', " ", tweet)
-            tweet = No_punct(tweet)
-            #tweet = Lemmas(tweet)
-            tweet = Party_Catcher(tweet)
-            tweet = Hellenize_Names(tweet)
-            #tweet = tweet.lower().lstrip()
+            tweet = re.sub(
+                r'''(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))''',
+                " ", tweet)
+            tweet = no_punctuation(tweet)
+            # tweet = Lemmas(tweet)
+            tweet = party_catcher(tweet)
+            tweet = hellenize_names(tweet)
+            # tweet = tweet.lower().lstrip()
             tweet = tweet.lstrip()
             tweet = tweet.lower()
-            tweet = Pitchless(tweet)
+            tweet = pitchless(tweet)
             tweet = " ".join([i for i in tweet.split() if i not in stop])
-            #tweet = word_tokenize(tweet)
-            #doc_complete.append(tweet)
+            # tweet = word_tokenize(tweet)
+            # doc_complete.append(tweet)
             tweet = re.sub('[^α-ω]', ' ', tweet)
             tweet = " ".join(tweet.split())
-            tweet = Normalize_Names(tweet)
-            #tweet = " ".join([i for i in tweet.split() if len(i)>1 and i not in exception])
+            tweet = normalize_names(tweet)
+            # tweet = " ".join([i for i in tweet.split() if len(i)>1 and i not in exception])
             all_prop.write(tweet)
 all_prop.close()
 
--- collect-data/retrieve_links.py
+++ collect-data/retrieve_links.py
@@ -5,14 +5,13 @@ import re, glob
 import time
 
 
-def dictWriter(dictionary):
+def dict_writer(dictionary):
     x = input("Enter file name ")
-    file = open(x, 'a', encoding = 'utf-8')
+    file = open(x, 'a', encoding='utf-8')
     for i, k in dictionary.items():
         file.write(str(i) + " : " + str(k))
         file.write("\n")
     file.close()
-
 
 
 ''''
@@ -62,7 +61,6 @@ for i in link_list[2801:3000]:
 print(counter)
 '''
 
-
 link_list = []
 for name in glob.glob("C:/Users/johnp/Downloads/GR_Lost/*"):
     html = urlopen("file:///" + name)
@@ -86,23 +84,21 @@ print("List completed")
 
 print(len(link_list))
 wikt_dict = {}
-#o = []
-
-
+# o = []
 
 
 for i in link_list[1:1000]:
     try:
         time.sleep(0.08)
-        html = urlopen("https://el.wiktionary.org/"+i)
+        html = urlopen("https://el.wiktionary.org/" + i)
         soup = bs(html, 'html.parser')
         title = soup.title.get_text()
         title = re.sub(r'\b - Βικιλεξικό\b', '', title)
-        #try:
-         #   title = soup.title.get_text()
-          #  title = re.sub(r'\b - Βικιλεξικό\b', '', title)
-        #except AttributeError:
-         #   title = ""
+        # try:
+        #   title = soup.title.get_text()
+        #  title = re.sub(r'\b - Βικιλεξικό\b', '', title)
+        # except AttributeError:
+        #   title = ""
         for i in soup.find_all('tbody'):
             if "ονομαστική" in i.get_text().split():
                 temp = i.get_text()
@@ -114,47 +110,58 @@ for i in link_list[1:1000]:
                 temp = re.sub(r'\bενικός\b', ' ', temp)
                 temp = re.sub(r'\bπληθυντικός\b', ' ', temp)
                 temp = re.sub(r'\bπτώση\b', ' ', temp)
-                #o.append(set(temp.split())
+                # o.append(set(temp.split())
                 for j in set(temp.split()):
                     if j != title:
-                        wikt_dict.update({j : title})
+                        wikt_dict.update({j: title})
             if "Ενεστώτας" in i.get_text().split() and "Μέση-Παθητική" not in i.get_text().split():
                 temp = i.get_text()
                 temp = re.sub(r'[^α-ωΑ-Ωά-ώΆ-Ώΐΰ]', ' ', temp)
                 temp = re.sub(r'\bθα\b|\bνα\b', ' ', temp)
                 temp = re.sub(r'\bέχω\b|\bέχεις\b|\bέχει\b|\bέχουμε\b|\bέχετε\b|\bέχουν\b|\bέχουνε\b', ' ', temp)
                 temp = re.sub(r'\bείμαι\b|\bείσαι\b|\bείναι\b|\bείμαστε\b|\bείστε\b', ' ', temp)
-                temp = re.sub(r'\bείχα\b|\bείχες\b|\bείχε\b|\bείχαμε\b|\bείχατε\b|\bείχαν\b|\bείχανε\b|\bέχε\b|\bέχοντας\b', ' ', temp)
+                temp = re.sub(
+                    r'\bείχα\b|\bείχες\b|\bείχε\b|\bείχαμε\b|\bείχατε\b|\bείχαν\b|\bείχανε\b|\bέχε\b|\bέχοντας\b', ' ',
+                    temp)
                 temp = re.sub(r'\bήμουν\b|\bήσουν\b|\bήταν\b|\bήμασταν\b|\bήσασταν\b', ' ', temp)
                 temp = re.sub(r'\bΣυνοπτικοί\b|\bΕξακολουθητικοί\b|\bΣυντελεσμένοι\b|\bχρόνοι\b', ' ', temp)
                 temp = re.sub(r'\bενικ\b|\bβ\b|\bα\b|\bπληθ\b|\bγ\b|\bπρόσωπα\b', ' ', temp)
                 temp = re.sub(r'\bΠροστακτική\b|\bΜετοχή\b|\bΑπαρέμφατο\b|\bΥποτακτική\b', ' ', temp)
-                temp = re.sub(r'\bΣυνοπ\b|\bΠαρατατικός\b|\bΠαρακείμενος\b|\bΣυντελ\b|\bΜέλλ\b|\bΕξ\b|\bΥπερσυντέλικος\b|\bΕνεστώτας\b|\bΑόριστος\b', ' ', temp)
+                temp = re.sub(
+                    r'\bΣυνοπ\b|\bΠαρατατικός\b|\bΠαρακείμενος\b|\bΣυντελ\b|\bΜέλλ\b|\bΕξ\b|\bΥπερσυντέλικος\b|\bΕνεστώτας\b|\bΑόριστος\b',
+                    ' ', temp)
                 for j in set(temp.split()):
                     if j != title:
-                        wikt_dict.update({j : title})
+                        wikt_dict.update({j: title})
                 try:
-                    if "Ενεστώτας" in soup.tbody.find_next('tbody').get_text().split() and "Μέση-Παθητική" not in soup.tbody.find_next('tbody').get_text().split() and "Ευκτική" not in soup.tbody.find_next('tbody').get_text().split():
+                    if "Ενεστώτας" in soup.tbody.find_next(
+                            'tbody').get_text().split() and "Μέση-Παθητική" not in soup.tbody.find_next(
+                            'tbody').get_text().split() and "Ευκτική" not in soup.tbody.find_next(
+                            'tbody').get_text().split():
                         temp = re.sub(r'[^α-ωΑ-Ωά-ώΆ-Ώΐΰ]', ' ', temp)
                         temp = re.sub(r'\bθα\b|\bνα\b', ' ', temp)
-                        temp = re.sub(r'\bέχω\b|\bέχεις\b|\bέχει\b|\bέχουμε\b|\bέχετε\b|\bέχουν\b|\bέχουνε\b', ' ', temp)
+                        temp = re.sub(r'\bέχω\b|\bέχεις\b|\bέχει\b|\bέχουμε\b|\bέχετε\b|\bέχουν\b|\bέχουνε\b', ' ',
+                                      temp)
                         temp = re.sub(r'\bείμαι\b|\bείσαι\b|\bείναι\b|\bείμαστε\b|\bείστε\b', ' ', temp)
-                        temp = re.sub(r'\bείχα\b|\bείχες\b|\bείχε\b|\bείχαμε\b|\bείχατε\b|\bείχαν\b|\bείχανε\b|\bέχε\b|\bέχοντας\b', ' ', temp)
+                        temp = re.sub(
+                            r'\bείχα\b|\bείχες\b|\bείχε\b|\bείχαμε\b|\bείχατε\b|\bείχαν\b|\bείχανε\b|\bέχε\b|\bέχοντας\b',
+                            ' ', temp)
                         temp = re.sub(r'\bήμουν\b|\bήσουν\b|\bήταν\b|\bήμασταν\b|\bήσασταν\b', ' ', temp)
                         temp = re.sub(r'\bΣυνοπτικοί\b|\bΕξακολουθητικοί\b|\bΣυντελεσμένοι\b|\bχρόνοι\b', ' ', temp)
                         temp = re.sub(r'\bενικ\b|\bβ\b|\bα\b|\bπληθ\b|\bγ\b|\bπρόσωπα\b', ' ', temp)
                         temp = re.sub(r'\bΠροστακτική\b|\bΜετοχή\b|\bΑπαρέμφατο\b|\bΥποτακτική\b', ' ', temp)
-                        temp = re.sub(r'\bΣυνοπ\b|\bΠαρατατικός\b|\bΠαρακείμενος\b|\bΣυντελ\b|\bΜέλλ\b|\bΕξ\b|\bΥπερσυντέλικος\b|\bΕνεστώτας\b|\bΑόριστος\b', ' ', temp)
+                        temp = re.sub(
+                            r'\bΣυνοπ\b|\bΠαρατατικός\b|\bΠαρακείμενος\b|\bΣυντελ\b|\bΜέλλ\b|\bΕξ\b|\bΥπερσυντέλικος\b|\bΕνεστώτας\b|\bΑόριστος\b',
+                            ' ', temp)
                         for j in set(temp.split()):
                             if j != title:
-                                wikt_dict.update({j : title})
+                                wikt_dict.update({j: title})
                 except AttributeError:
                     pass
-                #o.append(set(temp.split())
+                # o.append(set(temp.split())
     except urllib.error.HTTPError:
         pass
     except urllib.error.URLError:
         pass
 
     # var = dd[var]
-
